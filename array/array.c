1b1ffe6ccdb1f4fbe24784e1139d07e3fb423236
Madison Nicole <maddie0264@gmail.com>
2024-05-30 12:42:20 -0400

    Create array.c

---
+++ array/array.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int length;
+
+    do
+    {
+        length = get_int("Length: ");
+    }
+    while (length < 1);
+
+    int array[length];
+    array[0] = 1;
+    printf("%i\n", array[0]);
+
+    for (int i = 1; i < length; i++)
+    {
+        array[i] = 2 * (array[i - 1]);
+        printf("%i\n", array[i]);
+    }
+}
